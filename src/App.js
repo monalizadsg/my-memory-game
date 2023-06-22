134dac900c24ad32d652da210bbde809e44a7d24
Monaliza Dasig <monalizadasig@gmail.com>
2023-06-21 19:28:12 -0700

    display front and back images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/apple.png" },
+  { src: "/img/blueberry.png" },
+  { src: "/img/cherry.png" },
+  { src: "/img/passion-fruit.png" },
+  { src: "/img/pineapple.png" },
+  { src: "/img/strawberry.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className='App'>
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card front' />
+              <img className='back' src='/img/cover.png' alt='card back' />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
